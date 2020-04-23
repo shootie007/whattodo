e9a6cc8c199fabb00fdb2eee6cd376fd7755c722
多田秀人 <shoot@Shootie-MacBook.local>
2020-04-23 13:52:25 +0900

    initialize

---
+++ surrogatedecision/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SurrogatedecisionConfig(AppConfig):
+    name = 'surrogatedecision'
---
+++ surrogatedecision/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Subject:
+    name = models.Charfield(max_length=100)
+    reg_date = models.DateTimeField('register date')
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# class User:
+#     id = models.
+#     name = models.Charfield(max_length=20)
